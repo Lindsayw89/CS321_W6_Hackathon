65da084bd87d65552f9b49e0e09968daf34e3be6
tcampbe <tcampb@icloud.com>
2019-10-29 19:32:47 -0500

    api quiz model

--- QuizApp/ApiModels/QuizModel.cs
+++ QuizApp/ApiModels/QuizModel.cs
@@ -6,5 +6,10 @@ namespace QuizApp.ApiModels
     public class QuizModel
     {
         // TODO: create quiz model props
+        int Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; } 
+        string Instructions { get; set; }
+        IEnumerable<QuestionModel> Questions { get; set; }
     }
 }
